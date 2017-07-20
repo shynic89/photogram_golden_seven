4914232844241e388af323197e6c26bdeb864b2c
Nick Su <hsu0@chicagobooth.edu>
2017-07-20 00:30:06 +0000

    completed show

--- app/controllers/pictures_controller.rb
+++ app/controllers/pictures_controller.rb
@@ -1,29 +1,20 @@
 class PicturesController < ApplicationController
   def new_form
-    render("pic_templates/new_form.html.erb")
+      render("pic_templates/new_form.html.erb")
   end
-
   def create_row
-    render("pic_templates/create_row.html.erb")
-  end
-
+      render("pic_templates/new_row.html.erb")
+  end 
   def index
-    render("pic_templates/index.html.erb")
+      render("pic_templates/index.html.erb")
   end
-
   def show
-    render("pic_templates/show.html.erb")
+     
+    @pic = Photo.find(params["an_id"])
+    
+      render("pic_templates/show.html.erb")
   end
-
   def edit_form
-    render("pic_templates/edit_form.html.erb")
-  end
-
-  def update_row
-    render("pic_templates/update_row.html.erb")
-  end
-
-  def destroy_row
-    render("pic_templates/destroy_row.html.erb")
+      render("pic_templates/edit_form.html.erb")
   end
 end
