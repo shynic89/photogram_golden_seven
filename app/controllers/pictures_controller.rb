50a436819413c8a73071c8312759abfa3481a09e
Nick Su <hsu0@chicagobooth.edu>
2017-07-20 01:55:38 +0000

    solved test 1

--- app/controllers/pictures_controller.rb
+++ app/controllers/pictures_controller.rb
@@ -1,11 +1,20 @@
 class PicturesController < ApplicationController
   def new_form
+      
       render("pic_templates/new_form.html.erb")
   end
   def create_row
-      render("pic_templates/new_row.html.erb")
+      @new = Photo.new
+      @new.source = params["source_input"]
+      @new.caption = params["caption_input"]
+      @new.save
+      @pic = Photo.all
+      
+      render("pic_templates/create_row.html.erb")
   end 
   def index
+     @pictures = Photo.all
+     
       render("pic_templates/index.html.erb")
   end
   def show
@@ -15,6 +24,17 @@ class PicturesController < ApplicationController
       render("pic_templates/show.html.erb")
   end
   def edit_form
+      @photo_id = params["some_id"].to_i
+      @pic = Photo.find(@photo_id)
+      
       render("pic_templates/edit_form.html.erb")
+  end
+  def update_row
+      @id = params["the_id"].to_i
+      p = Photo.find(@id)
+      p.source =params["source_input"]
+      p.caption = params["caption_input"]
+      p.save
+     render("pic_templates/update_row.html.erb") 
   end
 end
